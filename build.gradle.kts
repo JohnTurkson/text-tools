683a917fbd12ce766aa684943d5ffd5f6957032b
John Turkson <git@johnturkson.com>
2021-01-31 17:37:55 -0800

    Add publishing information

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.21"
+    `maven-publish`
 }
 
 group = "com.johnturkson.tools"
@@ -11,4 +12,23 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "SpacePackages"
+            url = uri("https://maven.pkg.jetbrains.space/johnturkson/p/packages/public")
+            credentials {
+                username = System.getenv("SPACE_PUBLISHING_USERNAME")
+                password = System.getenv("SPACE_PUBLISHING_TOKEN")
+            }
+        }
+    }
 }
